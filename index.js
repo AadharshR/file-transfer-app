a8a491ea98ef3943eb37ea260226bac831257379
AadharshR <aadharshr6@gmail.com>
2022-05-15 20:46:44 +0530

    Added lint changes

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require ('express');
-const { transfer } = require('./transfer');
-const app = express();
-const bodyParser = require('body-parser');
+const express = require('express')
+const { transfer } = require('./transfer')
+const app = express()
+const bodyParser = require('body-parser')
 app.use(bodyParser.json())
-const PORT = 3000;
+const PORT = 3000
 
-app.post('/', transfer);
+app.post('/', transfer)
 
 app.listen(PORT, () => {
-    console.log("Application is listening on port:", PORT);
-});
+  console.log('Application is listening on port:', PORT)
+})
--- transfer.js
+++ transfer.js
@@ -2,126 +2,116 @@ const fs = require('fs')
 const util = require('util')
 const path = require('path')
 const copyFile = util.promisify(fs.copyFile)
-const rename = util.promisify(fs.rename);
-const unlink = util.promisify(fs.unlink);
+const rename = util.promisify(fs.rename)
+const unlink = util.promisify(fs.unlink)
 
-
-async function createFolderIfNotExists(newPath) {
-    if (!fs.existsSync(newPath)) {
-        fs.mkdirSync(newPath);
-    }
-    return;
+async function createFolderIfNotExists (newPath) {
+  if (!fs.existsSync(newPath)) {
+    fs.mkdirSync(newPath)
+  }
 }
 
-async function validateRequest(body) {
-    if (body.sourceFolder === null || body.sourceFolder === undefined) {
-        return {
-            message: "Source Folder is missing"
-        }
+async function validateRequest (body) {
+  if (body.sourceFolder === null || body.sourceFolder === undefined) {
+    return {
+      message: 'Source Folder is missing'
     }
-    if (body.destFolder === null || body.destFolder === undefined) {
-        return {
-            message: "Destination Folder is missing"
-        }
+  }
+  if (body.destFolder === null || body.destFolder === undefined) {
+    return {
+      message: 'Destination Folder is missing'
     }
-    if (body.patternToMatch === null || body.patternToMatch === undefined) {
-        return {
-            message: "Pattern is missing"
-        }
+  }
+  if (body.patternToMatch === null || body.patternToMatch === undefined) {
+    return {
+      message: 'Pattern is missing'
     }
-    return null;
+  }
+  return null
 }
 
-async function handleStatus(errors, res, validator) {
-    if (errors.length == 0 && validator.numberOfFiles == 0) {
-        return res.status(400).json({ "message": "There was no files to match with the given pattern" });
-    }
+async function handleStatus (errors, res, validator) {
+  if (errors.length === 0 && validator.numberOfFiles === 0) {
+    return res.status(400).json({ message: 'There was no files to match with the given pattern' })
+  }
 
-    if (errors.length == 0) {
-        return res.status(200).json({ "message": "Files are successfully moved" });
-    }
-
-    else if (errors.length >= 0) {
-        return res.status(500).json({ "message": "There were some errors moving these files", errors });
-    }
+  if (errors.length === 0) {
+    return res.status(200).json({ message: 'Files are successfully moved' })
+  } else if (errors.length >= 0) {
+    return res.status(500).json({ message: 'There were some errors moving these files', errors })
+  }
 };
 
-async function copyFiles(context) {
-    let {
-        files,
-        patternToMatch,
-        matchedFiles,
-        destFolder,
-        res,
-        sourceFolder
-    } = context;
-    files.forEach(async file => {
-        if (file.includes(patternToMatch)) {
-            matchedFiles++;
-            const destPath = path.join(destFolder, file)
-            try {
-                await createFolderIfNotExists(destFolder);
-                await rename(file, destPath)
-
-            } catch (err) {
-                try {
-                    if (err.code === 'EXDEV') {
-                        const completeCurrentPath = path.join(sourceFolder, file)
-                        await copyFile(completeCurrentPath, destPath);
-                        await unlink(completeCurrentPath);
-                    } else {
-                        console.log("error", err)
-                        throw err
-                    }
-                }
-
-                catch (e) {
-                    return res.status(500).json({ "message": "Something went wrong, check the logs" });
-                }
-
-            }
+async function copyFiles (context) {
+  let {
+    files,
+    patternToMatch,
+    matchedFiles,
+    destFolder,
+    res,
+    sourceFolder
+  } = context
+  files.forEach(async file => {
+    if (file.includes(patternToMatch)) {
+      matchedFiles++
+      const destPath = path.join(destFolder, file)
+      try {
+        await createFolderIfNotExists(destFolder)
+        await rename(file, destPath)
+      } catch (err) {
+        try {
+          if (err.code === 'EXDEV') {
+            const completeCurrentPath = path.join(sourceFolder, file)
+            await copyFile(completeCurrentPath, destPath)
+            await unlink(completeCurrentPath)
+          } else {
+            console.log('error', err)
+            throw err
+          }
+        } catch (e) {
+          return res.status(500).json({ message: 'Something went wrong, check the logs' })
         }
-
-    });
-    return matchedFiles;
+      }
+    }
+  })
+  return matchedFiles
 }
 
-async function transfer(req, res) {
-    const {
-        sourceFolder,
-        destFolder,
-        patternToMatch
-    } = req.body;
+async function transfer (req, res) {
+  const {
+    sourceFolder,
+    destFolder,
+    patternToMatch
+  } = req.body
 
-    const validationResponse = await validateRequest(req.body);
-    if (validationResponse) {
-        return res.status(400).json(validationResponse)
+  const validationResponse = await validateRequest(req.body)
+  if (validationResponse) {
+    return res.status(400).json(validationResponse)
+  }
+  fs.readdir(sourceFolder, async (err, files) => {
+    if (err) {
+      console.log('Error', err)
+      return res.status(500).json({ message: 'There was an error reading the directory' })
     }
-    fs.readdir(sourceFolder, async (err, files) => {
-        if (err) {
-            console.log("Error", err)
-            return res.status(500).json({ message: "There was an error reading the directory" })
-        }
-        let matchedFiles = 0;
-        const errors = [];
-        const contextBody = {
-            res,
-            matchedFiles,
-            files,
-            sourceFolder,
-            destFolder,
-            patternToMatch
-        }
-        let numberOfFiles  = await copyFiles(contextBody);
+    const matchedFiles = 0
+    const errors = []
+    const contextBody = {
+      res,
+      matchedFiles,
+      files,
+      sourceFolder,
+      destFolder,
+      patternToMatch
+    }
+    const numberOfFiles = await copyFiles(contextBody)
 
-        const validator = {
-            numberOfFiles
-        }
-        await handleStatus(errors, res, validator);
-
-    });
+    const validator = {
+      numberOfFiles
+    }
+    await handleStatus(errors, res, validator)
+  })
 }
 
 module.exports = {
-    transfer
+  transfer
 }
